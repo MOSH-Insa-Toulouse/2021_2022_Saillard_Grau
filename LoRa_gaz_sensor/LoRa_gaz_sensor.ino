f71ddc482fc304dc8b241dec8a5bc0d0eda90e8f
vgrau98 <victor.grau93@gmail.com>
2021-12-07 13:52:22 +0100

    Kicad improvements

--- LoRa_gaz_sensor/LoRa_gaz_sensor.ino
+++ LoRa_gaz_sensor/LoRa_gaz_sensor.ino
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <TheThingsNetwork.h>
 #include <SoftwareSerial.h>
 #include <rn2xx3.h>
@@ -26,7 +27,7 @@ uint16_t gas_value;
 
 int lora = 0;
 
-rn2xx3 myLora(loraSerial);
+//rn2xx3 myLora(loraSerial);
 
 void maRoutine() {
   Serial.println("ok");
@@ -36,16 +37,18 @@ void maRoutine() {
 
 void setup() {
 
+
   pinMode(gas_PIN, INPUT);
-  pinMode(2, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
   digitalWrite(2, LOW);
   analogWrite(PWM,10);
   
   gas_value_float=0;
-  gas_value=0;
+  gas_value=0;  
+
 
   Serial.begin(9600);
-  loraSerial.begin(9600);
+  //loraSerial.begin(9600);
   Serial.println("Start");
   pinMode(RST, OUTPUT);
   
@@ -54,12 +57,12 @@ void setup() {
   delay(500);
   digitalWrite(RST, HIGH);
 
-  myLora.autobaud();
+  //myLora.autobaud();
 
   Serial.println("RN483 version number: ");
-  Serial.println(myLora.sysver());
+  //Serial.println(myLora.sysver());
   
-  myLora.initABP(devAddr, appSKey, nwkSKey);
+  //myLora.initABP(devAddr, appSKey, nwkSKey);
   attachInterrupt(digitalPinToInterrupt(2), maRoutine, HIGH);
 
   
@@ -72,23 +75,27 @@ void setup() {
 
 void loop() {
 
-  gas_value_float=analogRead(gas_PIN);
-  Serial.println(gas_value_float);
+  //gas_value_float=analogRead(gas_PIN);
+  //Serial.println(gas_value_float);
 
  
   if(lora==1){
+    gas_value_float=analogRead(gas_PIN);
+    Serial.println(gas_value_float);
     uint32_t alert=1;
     byte tab_alert[2];
     tab_alert[0]=highByte(alert);
     tab_alert[1]=lowByte(alert);
-    myLora.txBytes(tab_alert, sizeof(tab_alert));
+   // myLora.txBytes(tab_alert, sizeof(tab_alert));
+   delay(5000);
+    lora=0;
   }
   gas_value=gas_value_float*100;
   byte payload[2];
   payload[0]=highByte(gas_value);
   payload[1]=lowByte(gas_value);
 
-  myLora.txBytes(payload, sizeof(payload));
+  //myLora.txBytes(payload, sizeof(payload));
   
 
 }
